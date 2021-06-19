e9f170eddda5d5b7fb782b0f6481186c30d04ffb
Yurii <juretz32@yandex.ru>
2021-06-19 11:44:57 +0300

    repls-map tweak
    - New keybinding for CIDER
    - New keybinding for run-shell
    - New keybinding for SLIME

--- init.el
+++ init.el
@@ -126,11 +126,11 @@
   (global-set-key (kbd "M-/") 'repls-map)
   (define-key 'repls-map (kbd "i") 'ielm)
   (define-prefix-command 'run-cider-map)
-  (define-key 'repls-map (kbd "c") 'run-cider-map)
-  (define-key 'run-cider-map (kbd "c") 'cider-connect)
+  (define-key 'repls-map (kbd "j") 'run-cider-map)
+  (define-key 'run-cider-map (kbd "k") 'cider-connect)
   (define-key 'run-cider-map (kbd "j") 'cider-jack-in)
-  (define-key 'repls-map (kbd "l") 'slime)
-  (define-key 'repls-map (kbd "s") 'run-shell)
+  (define-key 'repls-map (kbd "s") 'slime)
+  (define-key 'repls-map (kbd "/") 'run-shell)
   (define-key 'repls-map (kbd "p") 'run-python-with-venv))
 
 
