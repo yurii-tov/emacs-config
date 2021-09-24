2f7ad69e4b9bbedf261d4fa5c6c39630c55894a1
Yurii <juretz32@yandex.ru>
2021-09-24 12:03:42 +0300

    Remove unneeded stuff from modeline

--- init.el
+++ init.el
@@ -334,8 +334,7 @@
                                  "")))
                 mode-line-client
                 mode-line-modified
-                mode-line-remote
-                mode-line-frame-identification
+                " "
                 mode-line-buffer-identification
                 "   "
                 mode-line-position
