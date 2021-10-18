a42e978694a0d52cd5145b0b817dddee3e9bacd5
Yurii <juretz32@yandex.ru>
2021-10-18 16:40:08 +0300

    Implement tables prettifier as preoutput filter

--- init.el
+++ init.el
@@ -1287,70 +1287,40 @@ Process .+
 
 
 (defun make-sql-table-pprint-filter (table-parser)
-  `(lambda (string)
-     (message "payload: %s" (prin1-to-string string))
-     (if (and string
-              (not (string-match "\\*\\*\\* .* \\*\\*\\*" string))
-              (string-match "^select .*;$" (ring-ref comint-input-ring 0))) ;; if last command was 'select'...
-         (let ((current-command-index (cadr comint-input-ring)))
-           (unless (= current-command-index (car comint-last-command))
-             (setq-local comint-last-command (cons current-command-index ""))) ;; reset output accumulator if needed
-           (setq-local comint-last-command
-                       (cons current-command-index
-                             (concat (cdr comint-last-command)
-                                     string))) ;; accumulate another output chunk
-           (when (string-match comint-prompt-regexp string)
-             (let* ((payload-raw (cdr comint-last-command))
-                    (prompt-index (string-match comint-prompt-regexp payload-raw))
-                    (prompt (substring payload-raw prompt-index))
-                    (payload (string-trim (replace-regexp-in-string
-                                           comint-prompt-regexp
-                                           ""
-                                           payload-raw))))
-               (sql-reset-last-command)
-               (format "%s%s" (if (string-to-list payload)
-                                    (format "[%s]\n" payload)
-                                  "")
-                       prompt))))
-       string) ;; else return input unchanged
-
-
-
-
-     ;; ---
-     ;; (let* ((p (point))
-     ;;        (bounds (save-excursion
-     ;;                  (when (and (string-match "^select .*;$" (ring-ref comint-input-ring 0)) ;; if last command was 'select'...
-     ;;                             (= (line-number-at-pos p) ;; and cursor is at the same line as prompt...
-     ;;                                (line-number-at-pos
-     ;;                                 (save-excursion
-     ;;                                   (re-search-backward comint-prompt-regexp nil t))))
-     ;;                             (or (re-search-backward comint-prompt-regexp nil t 2)  ;; and we can move to previous prompt...
-     ;;                                 (goto-char (point-min)))
-     ;;                             (and (> (- (line-number-at-pos p) ;; and we have any meaningful output
-     ;;                                        (line-number-at-pos (point)))
-     ;;                                     1)
-     ;;                                  (not (string-match "\\*\\*\\* .* \\*\\*\\*"
-     ;;                                                     (buffer-substring (point) p)))))
-     ;;                    (end-of-line 2)
-     ;;                    (let* ((start (line-beginning-position))
-     ;;                           (end (progn (re-search-forward comint-prompt-regexp nil t)
-     ;;                                       (re-search-backward comint-prompt-regexp nil t)
-     ;;                                       (beginning-of-line)
-     ;;                                       (1- (point)))))
-     ;;                      (list start end))))))
-     ;;   (when bounds
-     ;;     (let* ((start (car bounds))
-     ;;            (end (cadr bounds))
-     ;;            (text (string-trim (buffer-substring start end)))
-     ;;            (table (funcall ',table-parser text)))
-     ;;       (when table
-     ;;         (let ((table-pretty (orgtbl-to-orgtbl (append (cons 'hline (cons (car table) (cons 'hline (cdr table)))) '(hline)) nil)))
-     ;;           (save-excursion
-     ;;             (goto-char start)
-     ;;             (delete-region start end)
-     ;;             (insert table-pretty)))))))
-     ))
+  (let ((prettify `(lambda (text)
+                     (let ((table (funcall ',table-parser text)))
+                       (when table
+                         (orgtbl-to-orgtbl (append (cons 'hline
+                                                         (cons (car table)
+                                                               (cons 'hline (cdr table))))
+                                                   '(hline))
+                                           nil))))))
+    `(lambda (string)
+       (if (and string
+                (not (string-match "\\*\\*\\* .* \\*\\*\\*" string))
+                (string-match "^select .*;$" (ring-ref comint-input-ring 0))) ;; if last command was 'select'...
+           (let ((current-command-index (cadr comint-input-ring)))
+             (unless (= current-command-index (car comint-last-command))
+               (setq-local comint-last-command (cons current-command-index ""))) ;; reset output accumulator if needed
+             (setq-local comint-last-command
+                         (cons current-command-index
+                               (concat (cdr comint-last-command)
+                                       string))) ;; accumulate another output chunk
+             (when (string-match comint-prompt-regexp string) ;; when we have prompt string in another output chunk, ...
+               (let* ((payload-raw (cdr comint-last-command)) ;; get all accumulated output...
+                      (prompt-index (string-match comint-prompt-regexp payload-raw))
+                      (prompt (substring payload-raw prompt-index))
+                      (payload (string-trim (replace-regexp-in-string
+                                             comint-prompt-regexp
+                                             ""
+                                             payload-raw)))) ;; then cut prompt from payload
+                 (sql-reset-last-command)
+                 (format "%s%s"
+                         (if (string-to-list payload) ;; if payload is non-empty...
+                             (format "%s\n" (or (funcall ,prettify payload) payload)) ;; then try to convert it into pretty table
+                           "")
+                         prompt))))
+         string)))) ;; else return input unchanged
 
 
 (defun sql-setup-pprint-tables ()
