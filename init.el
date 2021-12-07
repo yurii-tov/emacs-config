4ed101e2c5dd6a2a092f6008742ab02e5f5395ae
Yurii <juretz32@yandex.ru>
2021-12-07 12:13:48 +0300

    L for Lisp 😊

--- init.el
+++ init.el
@@ -135,7 +135,7 @@
   (define-prefix-command 'repls-map)
   (global-set-key (kbd "C-x i") 'repls-map)
   (define-key 'repls-map (kbd "i") 'ielm)
-  (define-key 'repls-map (kbd "c") 'slime)
+  (define-key 'repls-map (kbd "l") 'slime)
   (define-key 'repls-map (kbd "s") 'run-shell)
   (define-key 'repls-map (kbd "p") 'run-python-with-venv)
   (define-key 'repls-map (kbd "r") 'run-racket)
