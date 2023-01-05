99651ac6aa04fe1253bafee700e32b066cf67229
Yurii <juretz32@yandex.ru>
2023-01-05 21:32:45 +0300

    Improve hippie-expand-try-functions-list

--- init.el
+++ init.el
@@ -898,12 +898,14 @@
 
 
 (setq hippie-expand-try-functions-list
-      '(try-complete-file-name-partially
-        try-complete-file-name
+      '(try-expand-dabbrev-visible
         try-expand-all-abbrevs
-        try-expand-dabbrev-visible
         try-expand-line
         try-expand-list
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-dabbrev-from-kill
+        try-expand-whole-kill
         try-expand-line-all-buffers
         try-expand-list-all-buffers
         try-expand-dabbrev-all-buffers))
