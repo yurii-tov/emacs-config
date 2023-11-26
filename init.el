ba19172f989e97d8bacb0c68bb8717131d5cf17c
Yurii <juretz32@yandex.ru>
2023-11-26 16:22:01 +0300

    Update melpa url

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
 
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")))
+        ("melpa" . "https://melpa.org/packages/")))
 
 
 (let ((packages '(cider
