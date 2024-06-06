f4cbfeadfc01a435a51dfb4f3c9c9ea26cdfb355
Yurii <juretz32@yandex.ru>
2024-06-06 17:06:56 +0300

    Bring back prefix-aware history scrolling

--- init.el
+++ init.el
@@ -2115,6 +2115,36 @@ Example input:
 (bind-keys '("M-r" comint-browse-command-history) comint-mode-map)
 
 
+;; Use prefix-style matching when scrolling through history
+
+
+(defun comint-previous-input-prefixed (&optional n)
+  (interactive)
+  (unless (memq last-command '(comint-previous-input-prefixed
+                               comint-next-input-prefixed
+                               comint-history-isearch-backward-regexp))
+    (setq comint-matching-input-from-input-string
+          (buffer-substring
+           (or (marker-position comint-accum-marker)
+               (process-mark (get-buffer-process (current-buffer))))
+           (point))))
+  (comint-previous-matching-input
+   (format "^%s.*" (regexp-quote comint-matching-input-from-input-string))
+   (or n 1)))
+
+
+(defun comint-next-input-prefixed ()
+  (interactive)
+  (comint-previous-input-prefixed -1))
+
+
+(bind-keys '("M-p" comint-previous-input-prefixed
+             "M-n" comint-next-input-prefixed
+             "<up>" comint-previous-input-prefixed
+             "<down>" comint-next-input-prefixed)
+           comint-mode-map)
+
+
 ;; Company completion
 
 
