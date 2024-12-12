57bc56316fdcb3db9938f6f065ca04670fdafb79
Yurii <juretz32@yandex.ru>
2024-12-12 21:06:33 +0300

    Better project root detection

--- init.el
+++ init.el
@@ -2812,6 +2812,38 @@ Example input:
           (t (error "There is no formatting tools available")))))
 
 
+;; Better project root detection
+
+
+(defun project-find-project-file (dir)
+  (cl-loop for pattern in '("pom.xml" ; Java
+                            "*.iml"
+                            "build.xml"
+                            "build.gradle"
+                            "project.clj" ; Clojure
+                            "Cargo.toml" ; Rust
+                            "go.mod" ; Go
+                            "Makefile" ; C/C++
+                            "requirements.txt" ; Python
+                            "package.json"; Javascript
+                            )
+           for project-file = (locate-dominating-file
+                               dir
+                               (lambda (d)
+                                 (car (file-expand-wildcards
+                                       (expand-file-name pattern d)))))
+           when project-file
+           return (cons 'project-file
+                        (file-name-directory project-file))))
+
+
+(cl-defmethod project-root ((project (head project-file)))
+  (cdr project))
+
+
+(setq project-find-functions '(project-try-vc project-find-project-file))
+
+
 ;; ==========
 ;; SQL client
 ;; ==========
