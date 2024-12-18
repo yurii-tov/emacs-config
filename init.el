dac3322e6aa4f3a70d57006209ae7c78ebce1617
Yurii <juretz32@yandex.ru>
2024-12-18 21:38:03 +0300

    Consider VCS-based method (the default) as the only good variant

--- init.el
+++ init.el
@@ -2813,28 +2813,6 @@ Example input:
           (t (error "There is no formatting tools available")))))
 
 
-;; Better project root detection
-
-
-(defun project-try-file (dir)
-  (cl-loop for pattern in '("*.iml" "pom.xml" "package.json" ".project")
-           for project-file = (locate-dominating-file
-                               dir
-                               (lambda (d)
-                                 (car (file-expand-wildcards
-                                       (expand-file-name pattern d)))))
-           when project-file
-           return (cons 'project-file
-                        (file-name-directory project-file))))
-
-
-(cl-defmethod project-root ((project (head project-file)))
-  (cdr project))
-
-
-(setq project-find-functions '(project-try-vc project-try-file))
-
-
 ;; ==========
 ;; SQL client
 ;; ==========
