0b3f7eefd2530e6fe621414f68c632d43dba8156
Yurii <juretz32@yandex.ru>
2025-03-05 14:12:23 +0300

    Remove org-mode preloading

--- init.el
+++ init.el
@@ -2423,16 +2423,6 @@ Example input:
       (goto-char (1- (org-table-end))))))
 
 
-;; Preload org-mode at startup
-
-
-(defun preload-org-mode ()
-  (with-temp-buffer (org-mode)))
-
-
-(add-hook 'emacs-startup-hook 'preload-org-mode)
-
-
 ;; ==============
 ;; shell commands
 ;; ==============
