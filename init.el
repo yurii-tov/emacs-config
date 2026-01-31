a60f32ee7283afb8f413a7eff7fa804fa8fbc950
Yurii <juretz32@yandex.ru>
2026-01-31 08:49:59 +0300

    Use :override advice on ido-get-work-directory

--- init.el
+++ init.el
@@ -869,16 +869,37 @@
 
 
 (advice-add 'ido-get-work-directory
-            :around
-            (lambda (f &rest args)
-              "Filter out disconnected remote directories"
-              (let ((ido-work-directory-list
-                     (cl-remove-if
-                      (lambda (x)
-                        (and (file-remote-p x)
-                             (not (file-remote-p x nil t))))
-                      ido-work-directory-list)))
-                (apply f args))))
+            :override
+            (lambda (&optional incr must-match)
+              "Filter out disconnected remote paths"
+              (let ((n (length ido-work-directory-list))
+                    (i ido-work-directory-index)
+                    (j 0)
+                    dir)
+                (if (or (not ido-text) (= (length ido-text) 0))
+                    (setq must-match nil))
+                (while (< j n)
+                  (setq i (+ i incr)
+                        j (1+ j))
+                  (if (> incr 0)
+                      (if (>= i n) (setq i 0))
+                    (if (< i 0) (setq i (1- n))))
+                  (setq dir (nth i ido-work-directory-list))
+                  (if (and dir
+                           (not (equal dir ido-current-directory))
+                           (or (not (file-remote-p dir))
+                               (file-remote-p dir nil t))
+                           (file-directory-p dir)
+                           (or (not must-match)
+                               (ido-set-matches-1
+                                (if (eq ido-cur-item 'file)
+                                    (ido-make-file-list-1 dir)
+                                  (ido-make-dir-list-1 dir)))))
+                      (setq j n)
+                    (setq dir nil)))
+                (if dir
+                    (setq ido-work-directory-index i))
+                dir)))
 
 
 ;; Colors
