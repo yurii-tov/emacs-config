ad5ca740d8f7de0aa8ea963caa715402bc426fe4
Yurii <tovy@1c.ru>
2026-03-16 11:52:36 +0300

    ox-textile package

---
+++ elpa/ox-textile-20210919.1738/ox-textile-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ox-textile" "20210919.1738"
+  "Textile Back-End for Org Export Engine."
+  '((org "8.1"))
+  :url "https://github.com/yashi/org-textile"
+  :commit "92764235055bd1b51411d3e9490023bed7437d7b"
+  :revdesc "92764235055b"
+  :keywords '("org" "textile")
+  :authors '(("Yasushi SHOJI" . "yasushi.shoji@gmail.com"))
+  :maintainers '(("Yasushi SHOJI" . "yasushi.shoji@gmail.com")))
