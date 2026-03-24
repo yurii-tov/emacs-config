3ff58b8e49de54cf8489f7fe8c99992776b95edb
Yurii <tovy@1c.ru>
2026-03-24 13:40:42 +0300

    Adjust docstring

--- init.el
+++ init.el
@@ -1874,7 +1874,7 @@ Optionally, formats the buffer with COMMAND (if provided)"
 (advice-add 'async-shell-command
             :around
             (lambda (f &rest args)
-              "By default, don't popup"
+              "Don't show the buffer by default"
               (if-let* ((buffer (cadr args))
                         ((or current-prefix-arg
                              (get-buffer-window buffer))))
