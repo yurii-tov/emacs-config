f8f178590668413b38629ffe00c0be43305b048f
Yurii <no@email.xxx>
2026-07-31 21:18:14 +0300

    Small fix

--- init.el
+++ init.el
@@ -2030,8 +2030,7 @@ Optionally, formats the buffer with COMMAND (if provided)"
 
 (defun shell-setup-buffer (&optional arg)
   (or (shell-find-buffer default-directory)
-      (and arg
-           (generate-new-buffer (shell-buffer-name)))
+      (and arg (generate-new-buffer (shell-buffer-name)))
       (when-let* ((p (project-current)))
         (shell-find-buffer (project-root p) (project-buffers p)))
       (get-buffer-create (shell-buffer-name))))
