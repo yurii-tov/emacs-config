581c694c18ce0abc39186c6010cacd7e289f7300
Yurii <juretz32@yandex.ru>
2026-08-30 13:46:09 +0300

    Adjust text scaling commands

--- init.el
+++ init.el
@@ -116,39 +116,39 @@
 
 
 (define-keymap :keymap global-map
-  "<f2>" 'kmacro-keymap                 "C-M-l" 'move-line-down
-  "C-v" 'scroll-up-3                    "C-=" 'text-scale-increase
-  "M-v" 'scroll-down-3                  "C-M-=" 'text-scale-decrease
-  "M-c" 'text-edit-map                  "C-+" 'text-scale-reset
-  "M-o" 'other-window                   "C-x ," 'compile
-  "C-1" 'delete-other-windows           "C-x ." 'recompile
-  "C-2" 'split-window-below             "C-x =" 'hl-line-mode
-  "C-3" 'split-window-right             "C-x o" 'open-in-external-program
-  "C-0" 'delete-window                  "C-x i" 'what-cursor-position
-  "M-k" 'kill-whole-line                "C-x d" 'diff-map
-  "M-=" 'count-words                    "C-x b" 'bookmark-set
-  "M-q" 'hippie-expand                  "C-x B" 'bookmark-delete
-  "M-1" 'shell-command                  "C-x j" 'bookmark-jump
-  "M-!" 'async-shell-command            "C-x u" 'reopen-with-sudo
-  "M-2" 'enclose-text-quotes            "C-x l" 'eglot
-  "M-3" 'enclose-text-asterisks         "C-x p" 'pass
-  "M-4" 'enclose-text-apostrophes       "C-x C-b" 'ibuffer
-  "M-9" 'enclose-text-parenthesis       "C-x C-k" 'kill-buffer-and-window
-  "M-(" 'enclose-text-angle             "C-x C-=" 'display-line-numbers-mode
-  "M-0" 'enclose-text-square            "C-x C-l" 'gptel-menu
-  "M-)" 'enclose-text-curly             "C-c j" 'cider-start-map
-  "M-i" 'format-buffer                  "C-c k" 'sql-connect
-  "M-u" 'force-revert-buffer            "C-c i" 'ielm
-  "M-j" 'switch-to-buffer               "C-c s" 'ssh
-  "M-`" 'shell                          "C-c x" 'run-proxy
-  "M-g" 'goto-line                      "C-c d" 'serve-directory
-  "M-/" project-prefix-map              "C-c v" 'capture-video
-  "M-l" 'move-line-up                   "C-c h" 'hexl-mode
-  "M-M" 'mc/mark-all-dwim               "C-c c" 'org-capture
-  "M-N" 'mc/mark-next-like-this         "C-c a" 'org-agenda
-  "M-P" 'mc/mark-previous-like-this     "C-c o" 'org-push
-  "S-<down-mouse-1>" nil                "C-c O" 'org-pull
-  "S-<mouse-1>" 'mc/add-cursor-on-click "C-c w" 'watch-file)
+  "<f2>" 'kmacro-keymap             "S-<mouse-1>" 'mc/add-cursor-on-click
+  "C-v" 'scroll-up-3                "C-M-l" 'move-line-down
+  "M-v" 'scroll-down-3              "C-=" 'text-scale-increase
+  "M-c" 'text-edit-map              "C--" 'text-scale-decrease
+  "M-o" 'other-window               "C-x ," 'compile
+  "C-1" 'delete-other-windows       "C-x ." 'recompile
+  "C-2" 'split-window-below         "C-x =" 'hl-line-mode
+  "C-3" 'split-window-right         "C-x o" 'open-in-external-program
+  "C-0" 'delete-window              "C-x i" 'what-cursor-position
+  "M-k" 'kill-whole-line            "C-x d" 'diff-map
+  "M-=" 'count-words                "C-x b" 'bookmark-set
+  "M-q" 'hippie-expand              "C-x B" 'bookmark-delete
+  "M-1" 'shell-command              "C-x j" 'bookmark-jump
+  "M-!" 'async-shell-command        "C-x u" 'reopen-with-sudo
+  "M-2" 'enclose-text-quotes        "C-x l" 'eglot
+  "M-3" 'enclose-text-asterisks     "C-x p" 'pass
+  "M-4" 'enclose-text-apostrophes   "C-x C-b" 'ibuffer
+  "M-9" 'enclose-text-parenthesis   "C-x C-k" 'kill-buffer-and-window
+  "M-(" 'enclose-text-angle         "C-x C-=" 'display-line-numbers-mode
+  "M-0" 'enclose-text-square        "C-x C-l" 'gptel-menu
+  "M-)" 'enclose-text-curly         "C-c j" 'cider-start-map
+  "M-i" 'format-buffer              "C-c k" 'sql-connect
+  "M-u" 'force-revert-buffer        "C-c i" 'ielm
+  "M-j" 'switch-to-buffer           "C-c s" 'ssh
+  "M-`" 'shell                      "C-c x" 'run-proxy
+  "M-g" 'goto-line                  "C-c d" 'serve-directory
+  "M-/" project-prefix-map          "C-c v" 'capture-video
+  "M-l" 'move-line-up               "C-c h" 'hexl-mode
+  "M-M" 'mc/mark-all-dwim           "C-c c" 'org-capture
+  "M-N" 'mc/mark-next-like-this     "C-c a" 'org-agenda
+  "M-P" 'mc/mark-previous-like-this "C-c o" 'org-push
+  "S-<down-mouse-1>" nil            "C-c O" 'org-pull
+  "C-c w" 'watch-file)
 
 
 ;; Conflicts
@@ -483,11 +483,6 @@
   (define-keymap :keymap global-map
     "<wheel-down>" 'scroll-up-line
     "<wheel-up>" 'scroll-down-line))
-
-
-(defun text-scale-reset ()
-  (interactive)
-  (text-scale-set 0))
 
 
 ;; Buffers
