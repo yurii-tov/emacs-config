47e4ed6cadf281e85e67c15e82d2dcfb5bf8c061
Yurii <juretz32@yandex.ru>
2026-09-08 15:16:27 +0300

    Rewrite shell-mode's clone-buffer-hook

--- init.el
+++ init.el
@@ -1980,17 +1980,20 @@ Optionally, formats the buffer with COMMAND (if provided)"
 
 (add-hook 'shell-mode-hook
           (lambda ()
-            (add-hook 'clone-buffer-hook
-                      (lambda ()
-                        (when (file-remote-p default-directory)
-                          (kill-process)
-                          (run-with-timer
-                           0.5 nil (lambda ()
-                                     (shell (current-buffer))
-                                     (sit-for 0.2)
-                                     (comint-send-input)
-                                     (comint-clear-buffer)))))
-                      nil t)))
+            (let ((b (current-buffer)))
+              (add-hook 'clone-buffer-hook
+                        `(lambda ()
+                           (sit-for 0.1)
+                           (erase-buffer)
+                           (run-with-timer
+                            0.2 nil (lambda ()
+                                      (when (file-remote-p default-directory)
+                                        (kill-process)
+                                        (sit-for 0.2)
+                                        (shell (current-buffer))
+                                        (with-current-buffer ,b
+                                          (comint--indirect-cleanup))))))
+                        nil t))))
 
 
 ;; Ssh sessions
